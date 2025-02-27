1b6fb339b6560670781eaa9228fed5b09522ea65
Godswill omuka <omukagodswil@gmail.com>
2025-02-28 01:37:13 +0300

    Redirects users to the homepage if their role does not match the required role

---
+++ src/component/ProtectedRoute.js
@@ -0,0 +1,11 @@
+// src/components/ProtectedRoute.js
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+
+function ProtectedRoute({ role }) {
+ 
+}
+
+
+export default ProtectedRoute;
