501f016f9811a34b3a2ad80857146bf46fb73d20
Godswill omuka <omukagodswil@gmail.com>
2025-02-28 10:27:33 +0300

    included test dashboards to test the login and signup process

---
+++ src/App.js
@@ -0,0 +1,55 @@
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from './component/HomePage';
+import SignUp from './component/SignUp';
+import Login from './component/Login';
+import ForgotPassword from './component/ForgotPassword';
+import ResetPassword from './component/ResetPassword';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './component/ProtectedRoute';
+import UserDashboard from './component/UserDashboard'; 
+import AdminDashboard from './component/AdminDashboard'; 
+import './App.css';
+
+
+
+function App() {
+ return (
+   <AuthProvider>
+     <Router>
+       <Routes>
+         {/* Public Routes */}
+         <Route path="/" element={<HomePage />} />
+         <Route path="/signup/:role" element={<SignUp />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/forgot-password" element={<ForgotPassword />} />
+         <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+
+         {/* Protected Routes */}
+         <Route
+           path="/dashboard/user"
+           element={<ProtectedRoute role="user" />}
+         >
+           <Route index element={<UserDashboard />} />
+         </Route>
+
+
+         <Route
+           path="/dashboard/admin"
+           element={<ProtectedRoute role="admin" />}
+         >
+           <Route index element={<AdminDashboard />} />
+         </Route>
+
+
+         {/* Fallback Route (Optional) */}
+         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+       </Routes>
+     </Router>
+   </AuthProvider>
+ );
+}
+
+
+export default App;
---
+++ src/component/UserDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UserDashboard () {
+    return (
+        <div>
+            <p>welcome to UserDashboard</p>
+        </div>
+    )
+
+};
+
+export default  UserDashboard;
