43f14be0bc910a3fbc673efc7514e4776abb2d73
Godswill omuka <omukagodswil@gmail.com>
2025-03-01 14:59:38 +0300

    resolved codes ensuring everything works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // ✅ Use AuthProvider instead
+import { AuthProvider } from './context/AuthContext'; 
 import DashboardPage from './pages/DashboardPage';
 import './App.css';
 
